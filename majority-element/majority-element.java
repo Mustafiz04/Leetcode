5e139497d716ee127eaa4e40f9debb5d146bd77b
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2021-12-24 22:34:09 +0530

    Time: 1 ms (99.90%), Space: 44.9 MB (47.61%) - LeetHub

---
+++ majority-element/majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0, majority = 0;
+        for(int num : nums){
+            if(count == 0){
+                majority = num;
+            }
+            if( num == majority ){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return majority;
+    }
+}
