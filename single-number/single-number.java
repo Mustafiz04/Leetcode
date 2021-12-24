d89e9dba1ffc4d107ebe0878d35e6af1d5c958a0
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2021-12-24 22:32:51 +0530

    Time: 9 ms (25.95%), Space: 39.7 MB (30.03%) - LeetHub

---
+++ single-number/single-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums.length == 0) return 0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i: nums){
+            hm.put(i, hm.getOrDefault(i, 0) + 1);
+        }
+        for(Map.Entry<Integer, Integer> map :hm.entrySet()){
+            if( map.getValue() == 1 ){
+                return map.getKey();
+            }
+        }
+        return -1;
+    }
+}
