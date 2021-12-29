1889acbd41f38d59133d48d92fcd375f1ffe709b
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2021-12-29 23:59:09 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (20.49%) - LeetHub

--- search-insert-position/search-insert-position.java
+++ search-insert-position/search-insert-position.java
@@ -1,36 +1,35 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int n = nums.length;
-        if( nums[n-1] < target ) return n;
-        if( nums[0] > target ) return 0;
-        for(int i = 0; i<n; i++){
-            if( nums[i] >= target ){
-                return i;
-            }
-        }
-        return -1;
-    //     int start = binary(nums, n, target);
-    //     if( start == n ){
-    //         int end = binary(nums, n, target+1);
-    //         return end;
-    //     }
-    //     return start;
+        
+//         Linear approach
+        // if( nums[n-1] < target ) return n;
+        // if( nums[0] > target ) return 0;
+        // for(int i = 0; i<n; i++){
+        //     if( nums[i] >= target ){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        int start = binary(nums, n, target);
+
+        return start;
     }
     
     public int binary(int[] nums, int n, int k){
         int l = 0;
         int r = n-1;
-        int p = n;
         
         while( l <= r ){
             int mid = (l + r)/2;
-            if( nums[mid] >= k ) {
-                p = mid;
+            if( nums[mid] == k  ){
+                return mid;   
+            }else if( nums[mid] >= k ) {
                 r = mid-1;
             }else{
                 l = mid+1;
             }
         }
-        return p;
+        return l;
     }
 }
