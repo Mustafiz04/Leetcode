952562f7e3ebe82b5342efe015df2b9265db23ff
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2021-12-31 19:59:42 +0530

    Time: 6 ms (27.58%), Space: 40.9 MB (30.17%) - LeetHub

---
+++ find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> list=new ArrayList<>();
+    void inorder(TreeNode root){
+        if(root == null) return;
+        
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+    public int[] findMode(TreeNode root) {
+        inorder(root);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int max=0;
+        List<Integer> ans=new ArrayList<>();
+        for(int i:list){
+            if(!map.containsKey(i))
+                map.put(i,1);
+            else
+                map.put(i,map.get(i)+1);
+            if(map.get(i)>max)
+                max=map.get(i);
+        }
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            if(entry.getValue()==max)
+                ans.add(entry.getKey());
+        }
+        int a[]=new int[ans.size()];
+        for(int i=0;i<ans.size();i++)
+            a[i]=ans.get(i);
+        return a;
+        
+    }
+}
