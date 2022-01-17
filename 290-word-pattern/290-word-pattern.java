0bb2fb84c286c6e8bed3f9c5de797f94808c3b45
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-01-17 22:54:35 +0530

    Time: 5 ms (7.09%), Space: 37.5 MB (44.33%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -1,33 +1,23 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String words[] = s.split(" ");
-        
-        if(words.length != pattern.length()) return false; // base condition, if words length not equals to pattern length
-        // eg :- aaa != aa aa aa aa
-        
-        Map<Character, String> map1 = new HashMap<>(); // this hashmap assign the characters with words
-        Map<String, Boolean> map2 = new HashMap<>(); // in this map we will mark the words as used
-        
-        for(int i = 0; i < pattern.length(); i++){
-            char ch = pattern.charAt(i);
-            
-            if(map1.containsKey(ch) == false){ // if the pattern character has not been mapped
-                if(map2.containsKey(words[i]) == true){ // but the word which we will assign to i character has been used
-                    return false; // eg :- dog = a & now dog = b;
-                }
-                else{ // if the word has not been used;
-                    map2.put(words[i], true); // now mark it as used
-                    map1.put(ch, words[i]); // and put that word infront of character
-                }
+        HashMap<String, Character> map = new HashMap<>();
+        int[] vis = new int[26];
+        String[] words = s.split(" ");
+        if( words.length > pattern.length() ) return false;
+        int i = 0;
+        String pat = "";
+        for(String word : words){
+            if( map.containsKey(word) ){
+                pat += map.get(word);
+            }else{
+                char curr = pattern.charAt(i);
+                if( vis[curr - 'a'] == 1 ) return false;
+                map.put(word, curr);
+                pat += pattern.charAt(i);
+                vis[curr - 'a'] = 1;
             }
-            else{ 
-                // if that character has already been mapped, now check with whom it has been mapped
-                String mwith = map1.get(ch);
-                // if mwith word is not equals to words[i], means already mapped with someone, then return false; 
-                // eg :- a = dog & now a = cat
-                if(mwith.equals(words[i]) == false) return false;
-            }
+            i++;
         }
-        return true;
+        return pattern.equals(pat);
     }
 }
