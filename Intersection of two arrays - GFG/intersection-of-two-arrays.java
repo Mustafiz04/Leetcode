3cfaab155b44436ceb1539021bfc3df9f791a096
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-01-19 22:29:50 +0530

    Added solution - LeetHub

--- Intersection of two arrays - GFG/intersection-of-two-arrays.java
+++ Intersection of two arrays - GFG/intersection-of-two-arrays.java
@@ -50,21 +50,32 @@ class GFG {
 
 class Solution {
     public static int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
-        Arrays.sort(a);
-        Arrays.sort(b);
+        // Arrays.sort(a);
+        // Arrays.sort(b);
         HashSet<Integer> set = new HashSet<>();
-        int i = 0, j = 0;
-        while( i != n && j != m ){
-            if( a[i] == b[j] ){
-                set.add(a[i]);
-                i++;
-                j++;
-            }else if( a[i] < b[j] ){
-                i++;
-            }else{
-                j++;
+        // int i = 0, j = 0;
+        for(int i : a){
+            set.add(i);
+        }
+        int count = 0;
+        for(int j : b){
+            if( set.contains(j) ){
+                count++;
+                set.remove(j);
             }
         }
-        return set.size();
+        return count;
+        // while( i != n && j != m ){
+        //     if( a[i] == b[j] ){
+        //         set.add(a[i]);
+        //         i++;
+        //         j++;
+        //     }else if( a[i] < b[j] ){
+        //         i++;
+        //     }else{
+        //         j++;
+        //     }
+        // }
+        // return set.size();
     }
 };
