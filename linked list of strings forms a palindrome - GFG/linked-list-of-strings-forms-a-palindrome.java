2011e319e39e3406121582a649f283a31441cd1d
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-02-10 23:59:04 +0530

    Added solution - LeetHub

---
+++ linked list of strings forms a palindrome - GFG/linked-list-of-strings-forms-a-palindrome.java
@@ -0,0 +1,85 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+
+class Node
+{
+    String data;
+    Node next;
+    Node(String key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Palindrome
+{
+    static Node head;
+    public static void main (String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            String a = sc.next();
+            Node head = new Node(a);
+            addToTheLast(head);
+            for(int i = 1; i < n; i++)
+            {
+                String a1 = sc.next();
+                addToTheLast(new Node(a1));
+            }
+            
+            GfG gfg = new GfG();
+            boolean res = gfg.compute(head);
+            if(res == true)
+            System.out.println("True");
+            else
+            System.out.println("False");
+        }
+    }
+    
+    public static void addToTheLast(Node node)
+    {
+        if(head == null)
+        {
+            head = node;
+        }
+        else
+        {
+            Node temp = head;
+            while(temp.next != null)
+                 temp = temp.next;
+                 temp.next = node;
+        }
+    }
+}// } Driver Code Ends
+
+
+/*node class of the linked list
+class Node
+{
+    String data;
+    Node next;
+    Node(String key)
+    {
+        data = key;
+        next = null;
+    }
+}*/
+class GfG{
+    public static boolean compute(Node node){
+        String str = "";
+        while( node != null ){
+            str += node.data;
+            node = node.next;
+        }
+        int left = 0, right = str.length() - 1;
+        while( left < right ){
+            if( str.charAt(left++) != str.charAt(right--) ) return false;
+        } 
+        return true;
+    }
+}
