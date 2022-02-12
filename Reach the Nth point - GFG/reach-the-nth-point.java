d0f27fed115895f82cf8902047591e3030a2b50d
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-02-12 14:08:36 +0530

    Added solution - LeetHub

--- Reach the Nth point - GFG/reach-the-nth-point.java
+++ Reach the Nth point - GFG/reach-the-nth-point.java
@@ -25,15 +25,19 @@ class GFG
 //User function Template for Java
 
 class Solution{
+    int[] dp = new int[10001];
     public int nthPoint(int n){
         int MOD = 1000000007;
-        if( n <= 1 ) return n;
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i = 2; i<=n; i++){
-            dp[i] = ((dp[i-1])%MOD + (dp[i-2])%MOD)%MOD;
-        }
-        return dp[n];
+        // if( n <= 1 ) return n;
+        // int[] dp = new int[n+1];
+        // dp[0] = 1;
+        // dp[1] = 1;
+        // for(int i = 2; i<=n; i++){
+        //     dp[i] = ((dp[i-1])%MOD + (dp[i-2])%MOD)%MOD;
+        // }
+        // return dp[n];
+        if( n == 0 || n == 1 ) return 1;
+        if( dp[n] != 0 ) return dp[n];
+        return dp[n] = (nthPoint(n-1) + nthPoint(n-2) ) %MOD;
     }
 }
