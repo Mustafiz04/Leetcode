db457a7bc8bfdf22e5af1b6757bec5cc7d7e0e1f
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-02-15 23:05:00 +0530

    Added solution - LeetHub

---
+++ Longest consecutive subsequence - GFG/longest-consecutive-subsequence.java
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			
+			for(int i=0; i<n; i++)
+				a[i] = sc.nextInt();
+		    out.println(new Solution().findLongestConseqSubseq(a, n));
+		    t--;
+		}
+		out.flush();
+	}
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{ 
+	static int findLongestConseqSubseq(int arr[], int N){
+	    HashSet<Integer> set = new HashSet<>();
+	    for(int i : arr) set.add(i);
+	    
+	    int ans = 1;
+	    for(int i = 0; i<N; i++){
+	        int curr = arr[i];
+	        if( !set.contains(curr - 1) ){
+	            int j = curr;
+	            while( set.contains(j) ){
+	                j++;
+	            }
+	            ans = Math.max(ans, j - curr);
+	        }
+	    }
+	    return ans;
+	}
+}
