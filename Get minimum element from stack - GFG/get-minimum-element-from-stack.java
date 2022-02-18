b0b05b8f4f6ca6dc693b215b7ee3b09a4c1e0934
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-02-19 00:59:48 +0530

    Added solution - LeetHub

---
+++ Get minimum element from stack - GFG/get-minimum-element-from-stack.java
@@ -0,0 +1,88 @@
+// { Driver Code Starts
+import java.util.*;
+
+
+
+class Get_Min_From_Stack
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			int q = sc.nextInt();
+			GfG g = new GfG();
+			while(q>0)
+			{
+				int qt = sc.nextInt();
+				
+				if(qt == 1)
+				{
+					int att = sc.nextInt();
+					g.push(att);
+					//System.out.println(att);
+				}
+				else if(qt == 2)
+				{
+					System.out.print(g.pop()+" ");
+				}
+				else if(qt == 3)
+				{
+					System.out.print(g.getMin()+" ");
+				}
+			
+			q--;
+			}
+			System.out.println();
+		T--;
+		}
+		
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class GfG
+{
+    int minEle;
+    Stack<Integer> s;
+    
+    GfG(){
+		s = new Stack<Integer>();
+	}
+
+    int getMin(){
+        if( s.isEmpty() ) return -1;
+	    return minEle;
+    }
+    
+    int pop(){
+	    if( s.isEmpty() ) return -1;
+	    int y = s.peek();
+	    s.pop();
+	    if( y >= minEle ){
+	        return y;
+	    }else{
+	        minEle = 2 * minEle - y;
+	        return (y + minEle )/2;   
+	    }
+    }
+
+    void push(int x){
+	    if( s.isEmpty() ){
+	        s.push(x);
+	        minEle = x;
+	    }else {
+	        if( x >= minEle ){
+    	        s.push(x);
+    	    }else{
+    	        s.push(2*x - minEle);
+    	        minEle = x;
+    	    }
+	    }
+    }	
+}
+
