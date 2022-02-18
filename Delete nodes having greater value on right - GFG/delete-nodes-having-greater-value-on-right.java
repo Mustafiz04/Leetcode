b300a5affaa243a3213f6f34b26b2ca9a462555f
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-02-18 11:15:20 +0530

    Added solution - LeetHub

---
+++ Delete nodes having greater value on right - GFG/delete-nodes-having-greater-value-on-right.java
@@ -0,0 +1,114 @@
+// { Driver Code Starts
+import java.util.*;
+
+class Node
+{
+
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class LinkedList
+{
+    
+ 
+
+    /* Function to print linked list */
+   public static void print(Node root)
+    {
+        Node temp = root;
+        while(temp!=null)
+        {
+            System.out.print(temp.data + " ");
+            temp=temp.next;
+        }
+    }
+
+
+    /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+
+        /* Constructed Linked List is 1.2.3.4.5.6.
+         7.8.8.9.null */
+        int value;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0 && sc.hasNextInt() )
+        {
+            int n = sc.nextInt();
+            
+            //int n=Integer.parseInt(br.readLine());
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+
+            for (int i = 1; i < n && sc.hasNextInt() ; i++)
+            {
+                int a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+
+            Node result = new Solution().compute(head);
+            print(result);
+            System.out.println();
+            t--;
+        }
+    }
+}// } Driver Code Ends
+
+
+/*
+class Node {
+   int data;
+   Node next;
+
+  Node(int data) {
+      this.data = data;
+  }
+}
+*/
+class Solution{
+    Node compute(Node head){
+        head = reverseList(head);
+        head = _delLesserNodes(head);
+        return reverseList(head);
+    }
+    Node reverseList(Node head){
+        Node prev = null, next = null, curr = head;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    Node _delLesserNodes(Node head){
+        Node curr = head;
+        Node newLL = new Node(0);
+        Node temp = newLL;
+        temp.next = curr;
+        curr = curr.next;
+        temp = temp.next;
+        while(curr != null){
+            if(curr.data >= temp.data){
+                temp.next = curr;
+                temp = temp.next;
+            }
+            curr = curr.next;
+        }
+        temp.next = null;
+        return newLL.next;
+    }
+}
+  
