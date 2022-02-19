4862b8dcf330a5f477f1aaff9ccf606e4ca121f1
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-02-19 15:31:22 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -0,0 +1,72 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}// } Driver Code Ends
+
+
+class Node{
+    int node, parent;
+    Node(int node, int parent){
+        this.node = node;
+        this.parent = parent;
+    }
+}
+
+class Solution {
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj){
+        boolean[] vis = new boolean[V];
+        for(int i = 0; i< V; i++){
+            if( !vis[i] && isCycleBFS(i, adj, vis) ){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean isCycleBFS(int node, ArrayList<ArrayList<Integer>> adj, boolean[] vis ){
+        vis[node] = true;
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(node, -1) );
+        while(!q.isEmpty()){
+            Node curr = q.poll();
+            int currNode = curr.node;
+            int parent = curr.parent;
+            for(int next: adj.get(currNode) ){
+                if( !vis[next] ){
+                    vis[next] = true;
+                    q.add(new Node(next, currNode));
+                }else{
+                    if( parent != next ) return true;
+                }
+            }
+        }
+        return false;
+    }
+}
