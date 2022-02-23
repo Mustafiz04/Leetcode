0e0ff537fab7a228dc58513f5f9f19502e2ce6b4
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-02-24 00:14:46 +0530

    Added solution - LeetHub

---
+++ Queue using stack - GFG/queue-using-stack.java
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+import java.util.*;
+
+
+class Solution
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			Queue g = new Queue();
+			
+			int q = sc.nextInt();
+			while(q>0)
+			{
+				int QueryType = sc.nextInt();
+				if(QueryType == 1)
+				{
+					int a = sc.nextInt();
+					g.enqueue(a);
+				}
+				else if(QueryType == 2)
+				System.out.print(g.dequeue()+" ");
+			q--;
+			}	
+			System.out.println();
+				
+			
+			
+		t--;
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Queue
+{
+    Stack<Integer> input = new Stack<Integer>(); 
+    Stack<Integer> output = new Stack<Integer>(); 
+    
+    /*The method pop which return the element poped out of the stack*/
+    int dequeue(){
+        if( !output.isEmpty() ) return output.pop();
+        while( !input.isEmpty() ){
+            output.push(input.pop() );
+        }
+        if( output.isEmpty() ) return -1;
+        return output.pop();
+    }
+	
+    void enqueue(int x){
+        input.push(x);
+    }
+}
+
