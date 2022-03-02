67f7bc0466761706f3a870129eb38a70c1b936d7
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-03-02 09:54:15 +0530

    Time: 1 ms (86.22%), Space: 45.9 MB (35.60%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.java
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.java
@@ -18,23 +18,32 @@ class Node {
 */
 
 class Solution {
+    LinkedList<Integer> list = new LinkedList<>();
     public List<Integer> preorder(Node root) {
-        Stack<Node> st = new Stack<>();
-        LinkedList<Integer> ll = new LinkedList<>();
+        if(root == null){
+            return list;
+        }
+        list.add(root.val);
+        for(int i=0;i<root.children.size();i++){
+            preorder(root.children.get(i));
+        }
+        return list;
+//         Stack<Node> st = new Stack<>();
+//         LinkedList<Integer> ll = new LinkedList<>();
         
-        if( root == null ){
-            return ll;
-        }
-        st.add(root);
+//         if( root == null ){
+//             return ll;
+//         }
+//         st.add(root);
             
-        while( !st.isEmpty() ){
-            Node curr = st.pop();
-            ll.add(curr.val);
-            Collections.reverse(curr.children);
-            for( Node i : curr.children ){
-                st.add(i);
-            }
-        }
-        return ll;
+//         while( !st.isEmpty() ){
+//             Node curr = st.pop();
+//             ll.add(curr.val);
+//             Collections.reverse(curr.children);
+//             for( Node i : curr.children ){
+//                 st.add(i);
+//             }
+//         }
+//         return ll;
     }
 }
