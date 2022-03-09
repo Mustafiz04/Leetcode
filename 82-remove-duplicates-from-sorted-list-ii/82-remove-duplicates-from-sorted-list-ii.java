ec6763519c734679c81d42965fbb1fc3a7c9dad8
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-03-09 10:37:05 +0530

    Time: 1 ms (77.24%), Space: 44.9 MB (5.32%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.java
@@ -13,27 +13,43 @@ class Solution {
         if(head == null)
             return null;
         
-		Set<Integer> dup = new HashSet<>();
-		ListNode ll = head;
-		while(ll.next != null){
-			if(ll.val == ll.next.val)
-                dup.add(ll.val);
-			ll = ll.next;
-		}
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = head, prev = dummy;
+        while( curr != null ){
+            if( curr.next != null && curr.val == curr.next.val ){
+                while( curr.next != null && curr.val == curr.next.val ){
+                    curr = curr.next;
+                }
+                prev.next = curr.next;
+            }else{
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
         
-		ListNode dummy = new ListNode(0);
-		dummy.next = head;
-		ListNode ret = dummy;
+// 		Set<Integer> dup = new HashSet<>();
+// 		ListNode ll = head;
+// 		while(ll.next != null){
+// 			if(ll.val == ll.next.val)
+//                 dup.add(ll.val);
+// 			ll = ll.next;
+// 		}
         
-		while(head != null){
-			if( !dup.contains(head.val) ){
-				dummy.next = head;
-				dummy = dummy.next;
-			}
-			head = head.next;
-		}
+// 		ListNode dummy = new ListNode(0);
+// 		dummy.next = head;
+// 		ListNode ret = dummy;
         
-		dummy.next = null;
-		return ret.next;
+// 		while(head != null){
+// 			if( !dup.contains(head.val) ){
+// 				dummy.next = head;
+// 				dummy = dummy.next;
+// 			}
+// 			head = head.next;
+// 		}
+        
+// 		dummy.next = null;
+// 		return ret.next;
     }
 }
