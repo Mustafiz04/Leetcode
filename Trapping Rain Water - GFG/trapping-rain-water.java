5044b4810db451ec7727ce074c37c843ea29fd2f
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-03-09 12:03:11 +0530

    Added solution - LeetHub

--- Trapping Rain Water - GFG/trapping-rain-water.java
+++ Trapping Rain Water - GFG/trapping-rain-water.java
@@ -36,15 +36,23 @@ class Solution{
     static long trappingWater(int arr[], int n) { 
         int[] left = new int[n];
         int[] right = new int[n];
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i<n; i++ ){
-            if( arr[i] > max ) max = arr[i];
-            left[i] = max;
+        // int max = Integer.MIN_VALUE;
+        // for(int i = 0; i<n; i++ ){
+        //     if( arr[i] > max ) max = arr[i];
+        //     left[i] = max;
+        // }
+        // int min = Integer.MIN_VALUE;
+        // for(int i = n-1; i>=0; i-- ){
+        //     if( arr[i] > min ) min = arr[i];
+        //     right[i] = min;
+        // }
+        left[0] = arr[0];
+        for(int i = 1; i<n; i++){
+            left[i] = Math.max(left[i-1], arr[i]);
         }
-        int min = Integer.MIN_VALUE;
-        for(int i = n-1; i>=0; i-- ){
-            if( arr[i] > min ) min = arr[i];
-            right[i] = min;
+        right[n-1] = arr[n-1];
+        for(int i = n-2; i>=0; i--){
+            right[i] = Math.max(right[i+1], arr[i]);
         }
         long water = 0;
         for(int i = 0; i<n; i++){
