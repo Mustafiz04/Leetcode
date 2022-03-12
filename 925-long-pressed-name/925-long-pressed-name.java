895b78c07e58ab0b9425f3eed17bfd2c325079d1
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-03-12 21:13:37 +0530

    Time: 1 ms (83.95%), Space: 42.7 MB (6.25%) - LeetHub

--- 925-long-pressed-name/925-long-pressed-name.java
+++ 925-long-pressed-name/925-long-pressed-name.java
@@ -1,22 +1,10 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
-        int i = 0, j = 0, n = name.length(), m = typed.length();
-        if( m < n ) return false;
-        char prev = '-';
-        while( i < n && j < m ){
-            if( name.charAt(i) == typed.charAt(j) ){
-                prev = name.charAt(i);
-                i++;
-            }
-            if( typed.charAt(j) != prev ){
-                return false;
-            }
-            j++;
+        int c = 0;
+        for(int i=0; i<typed.length(); i++){
+            if(c < name.length() && name.charAt(c) == typed.charAt(i))   c++;
+            else if(i == 0 || typed.charAt(i) != typed.charAt(i-1))  return false;
         }
-        while( j < m ) {
-            System.out.println("HERER");
-            if(typed.charAt(j++) != name.charAt(n-1)) return false;
-        }
-        return i == n ? true : false;
+        return c == name.length();
     }
 }
