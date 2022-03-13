7396f8892c5124b27985d0811b219dd2210f4e3f
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-03-13 20:54:25 +0530

    Time: 2 ms (81.09%), Space: 42.3 MB (23.99%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,44 +1,35 @@
 class Solution {
     public boolean isValid(String s) {
         int n = s.length();
-        if( n == 0 ) return true;
-        Stack<Character> stack = new Stack<>();
-        
-        if(n % 2 == 1) return false;
-        
+        if( n % 2 != 0 ) return false;
+        Stack<Character> st = new Stack<>();
         for(int i = 0; i<n; i++){
             char curr = s.charAt(i);
-            if( curr == '(' || curr == '[' || curr == '{' ){
-                stack.push(curr);
+            if( curr == '(' || curr == '{' || curr == '[' ){
+                st.push(curr);
             }
-            
-            if( stack.isEmpty() ){
-                return false;
-            }
-            
+            if( st.isEmpty() ) return false;
             switch(curr){
                 case ')':
-                    curr = stack.pop();
-                    if( curr == '[' || curr == '{' ){
+                    curr = st.pop();
+                    if( curr == '{' || curr == '[' ){
                         return false;
                     }
                     break;
-                    
-                case ']':
-                    curr = stack.pop();
-                    if( curr == '(' || curr == '{' ){
-                        return false;
-                    }
-                    break;
-                    
                 case '}':
-                    curr = stack.pop();
-                    if( curr == '[' || curr == '(' ){
+                    curr = st.pop();
+                    if( curr == '(' || curr == '[' ){
+                        return false;
+                    }
+                    break;
+                case ']':
+                    curr = st.pop();
+                    if( curr == '{' || curr == '(' ){
                         return false;
                     }
                     break;
             }
         }
-        return stack.isEmpty();
+        return st.isEmpty();
     }
 }
