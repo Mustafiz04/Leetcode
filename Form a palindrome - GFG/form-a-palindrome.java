834db40fbcf06d01557c7e433913f276bc85cc88
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-03-24 22:57:18 +0530

    Added solution - LeetHub

--- Form a palindrome - GFG/form-a-palindrome.java
+++ Form a palindrome - GFG/form-a-palindrome.java
@@ -1,60 +1,60 @@
 // { Driver Code Starts
-import java.io.*;
-import java.util.*; 
+//Initial Template for Java
 
-class GFG{
-    public static void main(String args[]) throws IOException { 
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        
-        while(t-- > 0){
-            String str = read.readLine();
-
-            Solution ob = new Solution();
+        while(t-- > 0)
+        {
             
-            System.out.println(ob.countMin(str));
+            String S = read.readLine().trim();
+            Solution ob = new Solution();
+            System.out.println(ob.findMinInsertions(S));
         }
-    } 
-} // } Driver Code Ends
+    }
+}// } Driver Code Ends
 
 
 //User function Template for Java
 
 class Solution{
-    static int countMin(String str){
-        return str.length() - longestPalinSubseq(str);
+    int findMinInsertions(String S){
+        String T = reverse(S);
+        int n = S.length();
+        return n - LCS(S, T);
     }
     
-    public static int longestPalinSubseq(String S) {
-        int n = S.length();
-        String T = reverse(S);
-        int[][] dp = new int[n + 1][n + 1];
-    
-        for (int i = 1; i <= n; i++) {
-          for (int j = 1; j <= n; j++) {
-            if (S.charAt(i - 1) == T.charAt(j - 1)) {
-              dp[i][j] = 1 + dp[i - 1][j - 1];
-            } else {
-              dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+    int LCS(String s, String t){
+        int n = s.length();
+        int[][] dp = new int[n+1][n+1];
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                if( s.charAt(i-1) == t.charAt(j-1) ){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
             }
-          }
         }
         return dp[n][n];
     }
-
-    public static String reverse(String s) {
+    
+    String reverse(String s){
         char[] arr = s.toCharArray();
-        int l = 0, r = arr.length - 1;
-        while (l <= r) {
-          char temp = arr[l];
-          arr[l] = arr[r];
-          arr[r] = temp;
-          l++;
-          r--;
+        int i = 0, j = arr.length - 1;
+        while( i < j ){
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
         }
-        String ans = "";
-        for (char c : arr)
-          ans += c;
-        return ans;
+        return String.valueOf(arr);
     }
 }
