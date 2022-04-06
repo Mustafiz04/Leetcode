051ace66299cabc799418e26ed616482ebcb0f36
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-04-06 11:15:58 +0530

    Time: 4 ms (80.65%), Space: 44.5 MB (38.31%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int threeSumMulti(int[] A, int target) {
+        int MOD = 1000000007;
+        long[] count = new long[101];
+        int uniq = 0;
+        for (int x: A) {
+            count[x]++;
+            if (count[x] == 1)
+                uniq++;
+        }
+
+        int[] keys = new int[uniq];
+        int t = 0;
+        for (int i = 0; i <= 100; ++i)
+            if (count[i] > 0)
+                keys[t++] = i;
+
+        long ans = 0;
+
+        for (int i = 0; i < keys.length; ++i) {
+            int x = keys[i];
+            int T = target - x;
+            int j = i, k = keys.length - 1;
+            while (j <= k) {
+                int y = keys[j], z = keys[k];
+                if (y + z < T) {
+                    j++;
+                } else if (y + z > T) {
+                    k--;
+                } else {
+                    if (i < j && j < k) {
+                        ans += count[x] * count[y] * count[z];
+                    } else if (i == j && j < k) {
+                        ans += count[x] * (count[x] - 1) / 2 * count[z];
+                    } else if (i < j && j == k) {
+                        ans += count[x] * count[y] * (count[y] - 1) / 2;
+                    } else {  // i == j == k
+                        ans += count[x] * (count[x] - 1) * (count[x] - 2) / 6;
+                    }
+
+                    ans %= MOD;
+                    j++;
+                    k--;
+                }
+            }
+        }
+
+        return (int) ans;
+    }
+}
