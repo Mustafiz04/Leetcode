5696ce9e4852715969ff7f3ae92cbb085201ff1d
Mustafiz04 <kaifee000@gmail.com>
2023-04-06 14:35:11 +0530

    Added solution - CodeSyncer

---
+++ leetcode/1.-two-sum/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        int[] ans = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<n; i++){
+            if( map.containsKey(target - nums[i]) ){
+                ans[0] = i;
+                ans[1] = map.get(target - nums[i]);
+                return ans;
+            }
+            map.put(nums[i], i);
+        }
+        return ans;
+    }
+}
