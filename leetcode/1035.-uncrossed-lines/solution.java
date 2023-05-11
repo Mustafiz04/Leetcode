13ab8503134e68d08c58808cce37dfa9ab2d8bd5
Mustafiz04 <kaifee000@gmail.com>
2023-05-11 21:14:51 +0530

    Added solution - CodeSyncer

--- leetcode/1035.-uncrossed-lines/solution.java
+++ leetcode/1035.-uncrossed-lines/solution.java
@@ -1,19 +1,36 @@
 class Solution {
-    public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        int[][] dp = new int[n+1][m+1];
-
-        for(int i = 1; i<=n; i++) {
-            for(int j = 1; j<=m; j++){
-                if( nums1[i-1] == nums2[j-1] ) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                } else {
-                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+    boolean isValid = false;
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int row = grid1.length;
+        if(row == 0) return 0;
+        int col = grid1[0].length;
+        
+        int count = 0;
+        for(int i = 0; i<row; i++) {
+            for(int j = 0; j<col; j++) {
+                if( grid2[i][j] == 1 ) {
+                    isValid = true;
+                    dfs(grid1, grid2, i, j);
+                    if(isValid) count++;
                 }
             }
         }
+        return count;
+    }
 
-        return dp[n][m];
+    public void dfs(int[][] grid1, int[][] grid2, int i, int j) {
+        int row = grid2.length;
+        int col = grid2[0].length;
+        if( i < 0 || j < 0 || i >= row || j >= col || grid2[i][j] != 1 ) {
+            return;
+        }
+        if( grid1[i][j] == 0 && grid2[i][j] == 1 ) isValid = false;
+        grid2[i][j] = 0;
+
+        dfs(grid1, grid2, i+1, j);
+        dfs(grid1, grid2, i-1, j);
+        dfs(grid1, grid2, i, j+1);
+        dfs(grid1, grid2, i, j-1);
+
     }
 }
