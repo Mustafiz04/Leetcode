2ac28986a4f77212258c8b7c5f4e3cbcc0ea381b
Mustafiz04 <kaifee000@gmail.com>
2023-05-12 22:33:49 +0530

    Added solution - CodeSyncer

---
+++ leetcode/152.-maximum-product-subarray/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int maxProd = nums[0];
+        int minProd = nums[0];
+        int ans = maxProd;
+
+        for(int i = 1; i<n; i++) {
+            int curr = nums[i];
+            int tempProd = Math.max(curr, Math.max(maxProd * curr, minProd * curr));
+            minProd = Math.min(curr, Math.min(maxProd * curr, minProd * curr));
+            maxProd = tempProd;
+            ans = Math.max(ans, maxProd);
+        }
+        return ans;
+    }
+}
