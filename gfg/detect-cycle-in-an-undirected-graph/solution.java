e5e0b4a1e5a0fa2b94e09365a0ca4490608e90e3
Mustafiz04 <kaifee000@gmail.com>
2023-05-14 22:06:38 +0530

    Added solution - CodeHub

--- gfg/detect-cycle-in-an-undirected-graph/solution.java
+++ gfg/detect-cycle-in-an-undirected-graph/solution.java
@@ -1,26 +1,27 @@
-class Solution {
-    class Node {
-        int val, parent;
-        Node(int val, int parent) {
-            this.val = val;
-            this.parent = parent;
-        }
+...// } Driver Code Ends
+class Node {
+    int node, parent;
+    Node(int node, int parent) {
+        this.node = node;
+        this.parent = parent;
     }
-    
-    public boolean checkCycle(int src, ArrayList<ArrayList<Integer>> adj, boolean[] vis){
-        vis[src] = true;
+}
+class Solution {
+    public boolean checkCycle(int i, ArrayList<ArrayList<Integer>> adj, boolean[] vis) {
         Queue<Node> q = new LinkedList<>();
-        q.add(new Node(src, -1));
+        q.add(new Node(i, -1));
+        vis[i] = true;
+        
         while(!q.isEmpty()) {
-            Node currNode = q.poll();
-            int currVal = currNode.val;
-            int parent = currNode.parent;
+            Node curr = q.poll();
+            int node = curr.node;
+            int parent = curr.parent;
             
-            for(int adjNode: adj.get(currVal)) {
-                if( !vis[adjNode] ) {
-                    vis[adjNode] = true;
-                    q.add(new Node(adjNode, currVal));
-                } else if ( parent != adjNode ) {
+            for(int n: adj.get(node)) {
+                if( !vis[n] ) {
+                    q.add(new Node(n, node));
+                    vis[n] = true;
+                } else if( parent != n ) {
                     return true;
                 }
             }
@@ -28,15 +29,14 @@ class Solution {
         return false;
     }
     
-    // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
-        boolean[] vis = new boolean[V+1];
+        boolean[] vis = new boolean[V];
+        
         for(int i = 0; i<V; i++) {
-            if(!vis[i]) {
-                if(checkCycle(i, adj, vis)) return true;
+            if( !vis[i] ) {
+                if(checkCycle(i, adj, vis) ) {
+                    return true;
+                }
             }
         }
         
-        return false;
-    }
-}
