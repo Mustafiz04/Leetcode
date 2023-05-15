fde9660d758cc80423e35a193722d9d15a7ebe0e
Mustafiz04 <kaifee000@gmail.com>
2023-05-16 00:10:05 +0530

    Added solution - CodeSyncer

--- leetcode/547.-number-of-provinces/solution.java
+++ leetcode/547.-number-of-provinces/solution.java
@@ -1,36 +1,45 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-
+    public int makeConnected(int n, int[][] connections) {
+        if(connections.length < n-1){
+            return -1;
+        }
+        
+        ArrayList<ArrayList<Integer>> graph = buildingGraph(n , connections);
+        
+        int count = 0;
+        boolean[] visited = new boolean[n];
+        for(int i = 0; i < n; i++){
+            if(visited[i] == false){
+                count++;
+                DFS(i, graph, visited );
+            }
+        }
+        return count - 1;
+    }
+    
+    public static void DFS(int src , ArrayList<ArrayList<Integer>> graph , boolean[] visited){
+        visited[src] = true;
+        for(int element : graph.get(src)){
+            if(visited[element] == false){
+                DFS(element , graph , visited );
+            }
+        }
+    }
+    
+    public static ArrayList<ArrayList<Integer>> buildingGraph(int n , int[][] connections){
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i<n; i++) {
+        for(int i = 0; i < n; i++){
             graph.add(new ArrayList<>());
         }
-        for(int i = 0; i<n; i++) {
-            for(int j = 0; j<n; j++) {
-                if(i != j && isConnected[i][j] == 1) {
-                    graph.get(i).add(j);
-                }
-            }
-        }
 
-        boolean[] vis = new boolean[n];
-        int total = 0;
-        for(int i = 0; i<n; i++) {
-            if(!vis[i]) {
-                total++;
-                dfs(i, graph, vis);
-            }   
-        }
-        return total;
-    }
+        for(int[] row: connections){
+            int u = row[0];
+            int v = row[1];
 
-    public void dfs(int start, ArrayList<ArrayList<Integer>> graph, boolean[] vis){
-        vis[start] = true;
-        for(int neigh: graph.get(start)) {
-            if(!vis[neigh]) {
-                dfs(neigh, graph, vis);
-            }
+            graph.get(u).add(v);
+            graph.get(v).add(u);
         }
+        
+        return graph;
     }
 }
