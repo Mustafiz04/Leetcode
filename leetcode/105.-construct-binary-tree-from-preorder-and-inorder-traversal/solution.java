8229aa3d4f37acd727b897e41687fe7cd7bacb2b
Mustafiz04 <kaifee000@gmail.com>
2023-06-20 23:46:01 +0530

    Added solution - CodeSyncer

--- leetcode/105.-construct-binary-tree-from-preorder-and-inorder-traversal/solution.java
+++ leetcode/105.-construct-binary-tree-from-preorder-and-inorder-traversal/solution.java
@@ -1,35 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int[] getAverages(int[] nums, int k) {
-        int n = nums.length;
-
-        if( k == 0 ) return nums;
-        int[] ans = new int[n];
-        Arrays.fill(ans, -1);
-        int radius = 2*k + 1;
-        if(radius > n) return ans;
-
-        long sum = 0;
-        for(int i = 0; i< radius; i++) {
-            sum += nums[i];
+    HashMap<Integer, Integer> map = new HashMap<>();
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for(int i = 0; i<inorder.length; i++) {
+            map.put(inorder[i], i);
         }
-        ans[k] = (int) (sum /radius);
-
-        for(int i = radius; i<n; i++) {
-            sum = sum - nums[i - radius] + nums[i];
-            ans[i - k] = (int) (sum / radius);
-        }
-
-        // long[] prefix = new long[n+1];
-        // for(int i = 0; i<n; i++) {
-        //     prefix[i + 1] = prefix[i] + nums[i];
-        // }
-
-        // for(int i = k; i<(n-k); i++) {
-        //     int left = i -k, right = i + k;
-        //     long subArraySum = prefix[right + 1] - prefix[left];
-        //     int average = (int) (subArraySum) / radius;
-        //     ans[i] = average;
-        // }
-        return ans;
+        return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
+
+    public TreeNode buildTree(int[] preorder, int ps, int pe, int[] inorder, int is, int ie) {
+        if( ps > pe || is > ie ) return null;
+        TreeNode root = new TreeNode(preorder[ps]);
+        int nodeIndex = map.get(preorder[ps]);
+        int left = nodeIndex - is;
+        root.left = buildTree(preorder, ps+1, pe+left, inorder, is, nodeIndex-1);
+        root.right = buildTree(preorder, ps+left+1, pe, inorder, nodeIndex + 1, ie);
+        return root;
+    }    
 }
