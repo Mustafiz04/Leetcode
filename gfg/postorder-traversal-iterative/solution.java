81b23a15380020e8b4c11a1bc8be2e41e174d695
Mustafiz04 <kaifee000@gmail.com>
2023-06-23 21:56:18 +0530

    Added solution - CodeHub

--- gfg/postorder-traversal-iterative/solution.java
+++ gfg/postorder-traversal-iterative/solution.java
@@ -15,18 +15,17 @@ class Node{
 class Tree {
     ArrayList<Integer> postOrder(Node node) {
         ArrayList<Integer> ans = new ArrayList<>();
+        if( node == null ) return ans;
         Stack<Node> s = new Stack<>();
-        s.push(node);
         Stack<Node> s1 = new Stack<>();
+        s.push(node);
         while( !s.isEmpty() ) {
             Node currNode = s.pop();
             s1.push(currNode);
             if( currNode.left != null ) s.push(currNode.left);
             if( currNode.right != null ) s.push(currNode.right);
         }
-        while( !s1.isEmpty() ) {
-            ans.add(s1.pop().data);
-        }
+        while( !s1.isEmpty() ) ans.add(s1.pop().data);
         return ans;
     }
 }
