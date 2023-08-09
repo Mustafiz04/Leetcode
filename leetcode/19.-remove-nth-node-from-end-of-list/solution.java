8232192dc56a0d294e4312ec7e56f3efe9a6dca6
Mustafiz04 <kaifee000@gmail.com>
2023-08-09 23:26:52 +0530

    Added solution - CodeSyncer

--- leetcode/19.-remove-nth-node-from-end-of-list/solution.java
+++ leetcode/19.-remove-nth-node-from-end-of-list/solution.java
@@ -9,27 +9,24 @@
  * }
  */
 class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        if( head == null ) return null;
-        int len = lengthOfLL(head);
-        int nodeFromStart = len - n;
-        if( nodeFromStart == 0 ) {
-            return head.next;
-        }
-        ListNode curr = head;
-        while( nodeFromStart-- > 1 ) {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if( l1 == null & l2 == null ) return null;
+        if( l1 == null ) return l2;
+        if( l2 == null ) return l1;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+        while( l1 != null || l2 != null ){
+            int val1 = l1 != null ? l1.val : 0;
+            int val2 = l2 != null ? l2.val : 0;
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
             curr = curr.next;
+            if( l1 != null ) l1 = l1.next;
+            if( l2 != null ) l2 = l2.next;
         }
-        curr.next = curr.next.next;
-        return head;
-    }
-
-    public int lengthOfLL(ListNode head){
-        int len = 0;
-        while(head != null){
-            len++;
-            head = head.next;
-        }
-        return len;
+        if(carry != 0 ) curr.next = new ListNode(carry);
+        return dummy.next;
     }
 }
