bc3d9bb3c17366f9de02e11962dc4eb601c89ea7
Mustafiz04 <kaifee000@gmail.com>
2023-08-11 00:15:01 +0530

    Added solution - CodeSyncer

--- leetcode/33.-search-in-rotated-sorted-array/solution.java
+++ leetcode/33.-search-in-rotated-sorted-array/solution.java
@@ -1,25 +1,46 @@
 class Solution {
-    public int search(int[] nums, int target) {
+    public boolean search(int[] nums, int target) {
         int n = nums.length;
-        if( n == 0 ) return -1;
-        int left = 0, right = n-1;
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            if( nums[mid] == target ) return mid;
-            if( nums[mid] >= nums[left] ){
-                if( nums[mid] >= target && nums[left] <= target ){
-                    right = mid - 1;
-                }else{
-                    left = mid + 1;
+        if (n == 0) return false;
+        int end = n - 1;
+        int start = 0;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            if (!isBinarySearchHelpful(nums, start, nums[mid])) {
+                start++;
+                continue;
+            }
+            boolean pivotArray = existsInFirst(nums, start, nums[mid]);
+
+            boolean targetArray = existsInFirst(nums, start, target);
+            if (pivotArray ^ targetArray) {
+                if (pivotArray) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
                 }
-            }else{
-                if( nums[mid] <= target && nums[right] >= target ){
-                    left = mid + 1;
-                }else{
-                    right = mid - 1;
+            } else {
+                if (nums[mid] < target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
                 }
             }
         }
-        return -1;
+        return false;
+    }
+
+    private boolean isBinarySearchHelpful(int[] arr, int start, int element) {
+        return arr[start] != element;
+    }
+
+    private boolean existsInFirst(int[] arr, int start, int element) {
+        return arr[start] <= element;
     }
 }
