045b68bf3ab4d64df366e85cb8cb0d0b910b8f4f
Mustafiz04 <kaifee000@gmail.com>
2023-08-12 23:45:04 +0530

    Added solution - CodeSyncer

--- leetcode/51.-nqueens/solution.java
+++ leetcode/51.-nqueens/solution.java
@@ -1,21 +1,68 @@
 class Solution {
-    int[][] dp = new int[101][101];
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n = obstacleGrid.length;
-        if( n == 0 ) return 0;
-        int m = obstacleGrid[0].length;
-        return path(obstacleGrid, n, m);
+    List<List<String>> ans = new ArrayList<>();
+    
+    public List<List<String>> solveNQueens(int n) {
+        int[][] board = new int[n][n];
+        solveUtil(board, 0);
+        return ans;
     }
     
-    public int path(int[][] obstacleGrid, int n, int m){
-        if( n <= 0 || m <= 0 || obstacleGrid[n-1][m-1] == 1 ){
-            return 0;
+    public boolean solveUtil(int[][] board, int row){
+        int n = board.length;
+        if( row == n ){
+            print(board);
+            return true;
         }
-        if( n == 1 && m == 1 ){
-            return 1;
+        for(int col = 0; col<n; col++){
+            if( isSafe(board, row, col) ){
+                board[row][col] = 1;
+                solveUtil(board, row+1);
+                board[row][col] = 0;
+            }
         }
-        if( dp[n-1][m-1] != 0 ) return dp[n-1][m-1];
-        return dp[n-1][m-1] = path(obstacleGrid, n-1, m) + path(obstacleGrid, n, m-1);
-        
+        return false;
     }
+
+    private boolean isSafe(int[][] board, int row, int col) {
+        int N = board.length;
+        for (int i = 0; i < row; i++) 
+            if (board[i][col] == 1) 
+                return false; 
+
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) 
+            if (board[i][j] == 1) 
+                return false; 
+  
+        for (int i = row, j = col; j >= 0 && i < N; i++, j--) 
+            if (board[i][j] == 1) 
+                return false; 
+
+        for (int i = row, j = col; i < N && j < N; i++, j++) 
+            if (board[i][j] == 1) 
+                return false; 
+  
+        for (int i = row, j = col; j < N && i >= 0; i--, j++) 
+            if (board[i][j] == 1) 
+                return false; 
+
+        return true;
+    }
+
+    public void print(int[][] board){
+        List<String> r = new ArrayList<>();
+        int row = board.length;
+        for(int i = 0; i< row; i++){
+            String a = "";
+            for(int j = 0; j<row; j++ ){
+                if( board[i][j] == 0 ){
+                    a += ".";
+                }else{
+                    a += "Q";
+                }
+            }
+            r.add(a);
+        }
+        ans.add(r);
+    }
+    
 }
