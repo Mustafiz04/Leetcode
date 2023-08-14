5108d40447bdc63b1628db23814357a7db2e9c53
Mustafiz04 <kaifee000@gmail.com>
2023-08-15 00:56:50 +0530

    Added solution - CodeSyncer

--- leetcode/2.-add-two-numbers/solution.java
+++ leetcode/2.-add-two-numbers/solution.java
@@ -13,22 +13,28 @@ class Solution {
         if( l1 == null & l2 == null ) return null;
         if( l1 == null ) return l2;
         if( l2 == null ) return l1;
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        int carry = 0;
-        while( l1 != null || l2 != null ){
-            int val1 = l1 != null ? l1.val : 0;
-            int val2 = l2 != null ? l2.val : 0;
-            int sum = val1 + val2 + carry;
-            carry = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-            if( l1 != null ) l1 = l1.next;
-            if( l2 != null ) l2 = l2.next;
+        // ListNode dummy = new ListNode(0);
+        // ListNode curr = dummy;
+        // int carry = 0;
+        // while( l1 != null || l2 != null ){
+        //     int val1 = l1 != null ? l1.val : 0;
+        //     int val2 = l2 != null ? l2.val : 0;
+        //     int sum = val1 + val2 + carry;
+        //     carry = sum / 10;
+        //     curr.next = new ListNode(sum % 10);
+        //     curr = curr.next;
+        //     if( l1 != null ) l1 = l1.next;
+        //     if( l2 != null ) l2 = l2.next;
+        // }
+        // if(carry != 0 ) curr.next = new ListNode(carry);
+        // return dummy.next;
+
+        // Recursion
+        int sum = l1.val + l2.val;
+        ListNode nextNode = addTwoNumbers(l1.next, l2.next);
+        if( sum / 10 == 1 ) {
+            nextNode = addTwoNumbers(nextNode, new ListNode(1));
         }
-        if(carry != 0 ) curr.next = new ListNode(carry);
-        return dummy.next;
+        return new ListNode(sum % 10, nextNode);
     }
 }
-
-// To update you need to refresh the page and submit again.
