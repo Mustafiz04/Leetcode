21b618dbb5fcff7b672a7d80649fb97bba31711f
Mustafiz04 <kaifee000@gmail.com>
2023-08-17 20:36:27 +0530

    Added solution - CodeSyncer

--- leetcode/542.-01-matrix/solution.java
+++ leetcode/542.-01-matrix/solution.java
@@ -1,36 +1,47 @@
+class Node {
+    int x, y, step;
+    Node(int x, int y, int step) {
+        this.x = x;
+        this.y = y;
+        this.step = step;
+    }
+}
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
+    public int[][] updateMatrix(int[][] mat) {
+        int row = mat.length;
+        int col = mat[0].length;
+        boolean[][] vis = new boolean[row][col];
+        Queue<Node> q = new LinkedList<>();
 
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i<n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for(int i = 0; i<n; i++) {
-            for(int j = 0; j<n; j++) {
-                if(i != j && isConnected[i][j] == 1) {
-                    graph.get(i).add(j);
+        for(int i = 0; i<row; i++) {
+            for(int j = 0; j<col; j++) {
+                if( mat[i][j] == 0 ) {
+                    vis[i][j] = true;
+                    q.add(new Node(i, j, 0));
                 }
             }
         }
 
-        boolean[] vis = new boolean[n];
-        int total = 0;
-        for(int i = 0; i<n; i++) {
-            if(!vis[i]) {
-                total++;
-                dfs(i, graph, vis);
-            }   
-        }
-        return total;
-    }
+        int[] xdir = {1, -1, 0, 0};
+        int[] ydir = {0, 0, 1, -1};
 
-    public void dfs(int start, ArrayList<ArrayList<Integer>> graph, boolean[] vis){
-        vis[start] = true;
-        for(int neigh: graph.get(start)) {
-            if(!vis[neigh]) {
-                dfs(neigh, graph, vis);
+        while( !q.isEmpty() ) {
+            Node curr = q.poll();
+            int x = curr.x;
+            int y = curr.y;
+            int step = curr.step;
+            mat[x][y] = step;
+
+            for(int i = 0; i<4; i++) {
+                int newX = x + xdir[i];
+                int newY = y + ydir[i];
+                if( newX >= 0 && newX < row && newY >= 0 && newY < col && vis[newX][newY] == false ) {
+                    vis[newX][newY] = true;
+                    q.add(new Node(newX, newY, step+1));
+                }
             }
         }
+
+        return mat;
     }
 }
